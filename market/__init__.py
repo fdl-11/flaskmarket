db19a5eef72e499234dd1693201c2dce6c8035b8
fdl-11 <fadlidarusalam11@gmail.com>
2023-11-20 16:01:39 +0700

    many many change :v

--- market/__init__.py
+++ market/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = 'e4d6a4db475767f8257342'
 db = SQLAlchemy(app)
 
 from market import routes
